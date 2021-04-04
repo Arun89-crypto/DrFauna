d954cb32119d61e874309b199cf5c2872a853167
Arun Jangra <arunjngra89@gmail.com>
2021-04-04 17:42:05 +0530

    all done

--- javaScript/fetch.js
+++ javaScript/fetch.js
@@ -7,20 +7,38 @@ signBtn.addEventListener('click', () => {
     let signUpnumber = document.getElementById('signUpnumber').value;
     let signUptime = document.getElementById('signUptime').value;
     let signUpadd = document.getElementById('signUpadd').value;
-    obj = { name: signUpname, email: signUpemail, phone: signUpnumber, time: signUptime, address: signUpadd }
+    let obj = { name: signUpname, email: signUpemail, phone: signUpnumber, time: signUptime, address: signUpadd }
     console.log(obj);
+    collect(obj)
 })
-
+function collect (obj){
+    const options ={
+        method :'POST',
+        Headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(obj)
+    }
+    fetch('http://localhost:5500/drfauna/addngo',options);
+}
 
 // getting the values of the contact us 
 
-let contSubmitBtn = document.getElementById('contSubmitBtn');
-contSubmitBtn.addEventListener('click', () => {
+let contSubmit = document.getElementById('contSubmitBtn');
+contSubmit.addEventListener('click', () => {
     let contName = document.getElementById('contName').value;
     let contEmail = document.getElementById('contEmail').value;
     let contNumber = document.getElementById('contNumber').value;
     let contMessage = document.getElementById('contMessage').value;
 
     contactObj = { name: contName, email: contEmail, number: contNumber, message: contMessage }
+    const options ={
+        method :'POST',
+        Headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(contactObj)
+    }
+    fetch('http://localhost:5500/drfauna/contactus',options);
     console.log(contactObj);
 })
